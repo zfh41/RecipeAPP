d71a405374919c4b6b8eba7064f0e0e77be55a0b
EC2 Default User <ec2-user@ip-172-31-38-127.us-east-2.compute.internal>
2020-09-19 00:00:20 +0000

    Finished up retrieving random tweets related to food lookups, now will be adding the html

--- deliv1.py
+++ deliv1.py
@@ -1,10 +1,13 @@
+import os
+import flask
 from tweepy import OAuthHandler
 from tweepy import API
 from tweepy import Cursor
 from datetime import datetime, date, time, timedelta
 from collections import Counter
 import sys
-import os
+
+app = flask.Flask(__name__)
 
 consumer_key=os.environ["TWITTER_CONSUMER_KEY"]
 consumer_secret=os.environ["TWITTER_CONSUMER_SECRET"]
@@ -15,94 +18,30 @@ auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 auth_api = API(auth)
 
-account_list = []
-if (len(sys.argv) > 1):
-  account_list = sys.argv[1:]
-else:
-  print("Please provide a list of usernames at the command line.")
-  sys.exit(0)
+food_list = ["fried chicken", "ice cream", "salad", "lollipop", "mochi", "popcorn", "eggs"]
   
-if len(account_list) > 0:
-  for target in account_list:
-    # print("Getting data for " + target)
-    # item = auth_api.get_user(target)
-    # print("name: " + item.name)
-    # print("screen_name: " + item.screen_name)
-    # print("description: " + item.description)
-    # print("statuses_count: " + str(item.statuses_count))
-    # print("friends_count: " + str(item.friends_count))
-    # print("followers_count: " + str(item.followers_count))
-    
-    
-    hashtags = []
-    mentions = []
-    tweet_count = 0
+if len(food_list) > 0:
+  for target in food_list:
     end_date = datetime.utcnow() - timedelta(days=30)
-    for status in Cursor(auth_api.user_timeline, id=target).items():
-      print(status.text + "\n")
-      tweet_count += 1
-      # print(status)
-      if hasattr(status, "entities"):
-        entities = status.entities
-        if "hashtags" in entities:
-          for ent in entities["hashtags"]:
-            if ent is not None:
-              if "text" in ent:
-                hashtag = ent["text"]
-                if hashtag is not None:
-                  hashtags.append(hashtag)
-        if "user_mentions" in entities:
-          for ent in entities["user_mentions"]:
-            if ent is not None:
-              if "screen_name" in ent:
-                name = ent["screen_name"]
-                if name is not None:
-                  mentions.append(name)
+    tweets = Cursor(auth_api.search, q = target, lang="en").items(5)
+    tweetList=[]
+    userList=[]
+    dateList=[]
+    
+    
+    for status in tweets:
+      tweetList.append(status.text)
+      userList.append(status.user.screen_name)
+      dateList.append(str(status.created_at))
+      print("Tweet: " + status.text + " User: " + status.user.screen_name + " datecreated: " + str(status.created_at) + "\n")
       if status.created_at < end_date:
-        break
-      # print(status.text)
+          break
 
-    # tweets = item.statuses_count
-    # account_created_date = item.created_at
-    # delta = datetime.utcnow() - account_created_date
-    # account_age_days = delta.days
-    # print("Account age (in days): " + str(account_age_days))
-    # if account_age_days > 0:
-    #   print("Average tweets per day: " + "%.2f"%(float(tweets)/float(account_age_days)))
-    # hashtags = []
-    # mentions = []
-    # tweet_count = 0
-    # end_date = datetime.utcnow() - timedelta(days=30)
-    # for status in Cursor(auth_api.user_timeline, id=target).items():
-    #   tweet_count += 1
-    #   if hasattr(status, "entities"):
-    #     entities = status.entities
-    #     if "hashtags" in entities:
-    #       for ent in entities["hashtags"]:
-    #         if ent is not None:
-    #           if "text" in ent:
-    #             hashtag = ent["text"]
-    #             if hashtag is not None:
-    #               hashtags.append(hashtag)
-    #     if "user_mentions" in entities:
-    #       for ent in entities["user_mentions"]:
-    #         if ent is not None:
-    #           if "screen_name" in ent:
-    #             name = ent["screen_name"]
-    #             if name is not None:
-    #               mentions.append(name)
-    #   if status.created_at < end_date:
-    #     break
-    # print
-    # print("Most mentioned Twitter users:")
-    # for item, count in Counter(mentions).most_common(10):
-    #   print(item + "\t" + str(count))
+@app.route('/')
+def hello():
+    return flask.render_template("index.html", len = len(tweetList), tweetList = tweetList, userList=userList, dateList=dateList)
 
-    # print
-    # print("Most used hashtags:")
-    # for item, count in Counter(hashtags).most_common(10):
-    #   print(item + "\t" + str(count))
 
-    # print
-    # print("All done. Processed " + str(tweet_count) + " tweets.")
-    # print
+if __name__ == "__main__":
+    app.run(
+    port=int(os.getenv('PORT', 8080)), host=os.getenv('IP','0.0.0.0'))
