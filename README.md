71fd10c2707d4728589263363338d3dd2480b598
EC2 Default User <ec2-user@ip-172-31-38-127.us-east-2.compute.internal>
2020-09-20 15:51:21 +0000

    updated to show 7 foodsand used CSS file

---
+++ README.md
@@ -0,0 +1,20 @@
+This is a simple python project on generating food-related tweets
+
+APIs and Frameworks used: <br />
+Flask <br />
+Tweepy<br />
+python-dotenv <br />
+AWS Cloud9 <br />
+TwitterAPI <br /> <br />
+
+
+Follow this methodology in order to view the webpage with the tweets:
+0. First clone the repository using the git clone command: git clone https://github.com/NJIT-CS490/Proj1-Tweeting.git
+1. Once the repository is cloned, you can access the deliv1.py file. Run the python file using the python command: "python deliv1.py". 
+2. If you are using AWS cloud9, preview the webpage by using the preview dropdown and selecting "Preview Running Application".
+
+Problems Encountered:
+- Wasn't sure exactly what food tweets to captured. I felt it was not very specific in the guidelines as to whether the tweets had to be picked by the user and how specific the foods had to be.
+- Another issue I came across was that when I updated my CSS file, the same edits weren't being updated onto my page. 
+- I was unsure of how many tweets should be displayed onto the page. It was clarified on the slack channel that the number doesn't matter.
+- I was trying to figure out why one tweet rather than 7 was displaying for the my tweetList, but realized later that I forgot to initialize the array before the for loop :D
--- deliv1.py
+++ deliv1.py
@@ -19,37 +19,27 @@ auth.set_access_token(access_token, access_token_secret)
 auth_api = API(auth)
 
 food_list = ["fried chicken", "ice cream", "salad", "lollipop", "mochi", "popcorn", "eggs"]
-  
-<<<<<<< HEAD
-if len(food_list) > 0:
-  for target in food_list:
-    end_date = datetime.utcnow() - timedelta(days=30)
-    tweets = Cursor(auth_api.search, q = target, lang="en").items(5)
-    tweetList=[]
-    userList=[]
-    dateList=[]
-    
-    
-    for status in tweets:
-      tweetList.append(status.text)
-      userList.append(status.user.screen_name)
-      dateList.append(str(status.created_at))
-      print("Tweet: " + status.text + " User: " + status.user.screen_name + " datecreated: " + str(status.created_at) + "\n")
-      if status.created_at < end_date:
-          break
+tweetList=[]
+userList=[]
+dateList=[]
 
 @app.route('/')
-def hello():
-    return flask.render_template("index.html", len = len(tweetList), tweetList = tweetList, userList=userList, dateList=dateList)
 
+def index():
+    for target in food_list:
+      end_date = datetime.utcnow() - timedelta(days=30)
+      tweets = Cursor(auth_api.search, q = target, lang="en", tweet_mode=extended).items(1)
+      
+      for status in tweets:
+        tweetList.append(status.text)
+        userList.append(status.user.screen_name)
+        dateList.append(status.created_at.strftime('%m/%d/%Y, %H:%M:%S'))
+        
+        if status.created_at < end_date:
+            break
+      
+    return flask.render_template("index.html", len = len(tweetList), tweetList = tweetList, userList=userList, dateList=dateList)
 
 if __name__ == "__main__":
     app.run(
     port=int(os.getenv('PORT', 8080)), host=os.getenv('IP','0.0.0.0'))
-=======
-if len(account_list) > 0:
-  for target in account_list:
-    
-    for status in Cursor(auth_api.user_timeline, id=target).items():
-      print(status.text + "\n")
->>>>>>> b33132831cde14bcb35135a45539c3f6c64557ae
