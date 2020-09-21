03585f5c2c243365b352a5618db5277668866449
EC2 Default User <ec2-user@ip-172-31-38-127.us-east-2.compute.internal>
2020-09-21 18:54:26 +0000

    first milestone accomplished

--- deliv1.py
+++ deliv1.py
@@ -6,6 +6,7 @@ from tweepy import Cursor
 from datetime import datetime, date, time, timedelta
 from collections import Counter
 import sys
+import random
 
 app = flask.Flask(__name__)
 
@@ -18,28 +19,29 @@ auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 auth_api = API(auth)
 
-food_list = ["fried chicken", "ice cream", "salad", "lollipop", "mochi", "popcorn", "eggs"]
-tweetList=[]
-userList=[]
-dateList=[]
+
 
 @app.route('/')
 
 def index():
-    for target in food_list:
-      end_date = datetime.utcnow() - timedelta(days=30)
-      tweets = Cursor(auth_api.search, q = target, lang="en", tweet_mode=extended).items(1)
+    food_list = ["fried chicken", "ice cream", "salad", "lollipop", "mochi", "popcorn", "eggs"]
+    tweetList=[]
+    userList=[]
+    dateList=[]
+    randNum = random.randrange(0, 7)
+    end_date = datetime.utcnow() - timedelta(days=30)
+    tweets = Cursor(auth_api.search, q = food_list[randNum], lang="en").items(1)
       
-      for status in tweets:
+    for status in tweets:
         tweetList.append(status.text)
         userList.append(status.user.screen_name)
         dateList.append(status.created_at.strftime('%m/%d/%Y, %H:%M:%S'))
-        
+    
         if status.created_at < end_date:
             break
       
     return flask.render_template("index.html", len = len(tweetList), tweetList = tweetList, userList=userList, dateList=dateList)
 
-if __name__ == "__main__":
-    app.run(
-    port=int(os.getenv('PORT', 8080)), host=os.getenv('IP','0.0.0.0'))
+# if __name__ == "__main__":
+app.run(
+port=int(os.getenv('PORT', 8080)), host=os.getenv('IP','0.0.0.0'))
