98ef9b9de2fda66940cddce6388e9c5e0cf0168c
EC2 Default User <ec2-user@ip-172-31-38-127.us-east-2.compute.internal>
2020-09-26 20:33:42 +0000

    Added spoonacular API

--- README.md
+++ README.md
@@ -15,6 +15,6 @@ Follow this methodology in order to view the webpage with the tweets:
 
 Problems Encountered:
 - Wasn't sure exactly what food tweets to captured. I felt it was not very specific in the guidelines as to whether the tweets had to be picked by the user and how specific the foods had to be.
-- Another issue I came across was that when I updated my CSS file, the same edits weren't being updated onto my page. 
+- Another issue I came across was that when I updated my CSS file, the same edits weren't being updated onto my page. I resolved this by puttin
 - I was unsure of how many tweets should be displayed onto the page. It was clarified on the slack channel that the number doesn't matter.
 - I was trying to figure out why one tweet rather than 7 was displaying for the my tweetList, but realized later that I forgot to initialize the array before the for loop :D
--- deliv1.py
+++ deliv1.py
@@ -7,9 +7,18 @@ from datetime import datetime, date, time, timedelta
 from collections import Counter
 import sys
 import random
+import requests
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+import json
 
 app = flask.Flask(__name__)
 
+dotenv_path = join(dirname(__file__), 'twitter.env')
+load_dotenv(dotenv_path)
+
+spoonacular_key = os.environ['SPOONACULAR_KEY']
 consumer_key=os.environ["TWITTER_CONSUMER_KEY"]
 consumer_secret=os.environ["TWITTER_CONSUMER_SECRET"]
 access_token=os.environ["TWITTER_ACCEESS_TOKEN"]
@@ -25,12 +34,38 @@ auth_api = API(auth)
 
 def index():
     food_list = ["fried chicken", "ice cream", "salad", "lollipop", "mochi", "popcorn", "eggs"]
+    
+    
     tweetList=[]
     userList=[]
     dateList=[]
+    
+    ingredientList=[]
     randNum = random.randrange(0, 7)
     end_date = datetime.utcnow() - timedelta(days=30)
-    tweets = Cursor(auth_api.search, q = food_list[randNum], tweet_mode="extended", lang="en").items(1)
+    q = food_list[randNum]
+    
+    url = "https://api.spoonacular.com/recipes/findByIngredients?ingredients={}&apiKey={}".format(q, spoonacular_key)
+    
+    tweets = Cursor(auth_api.search, q, tweet_mode="extended", lang="en").items(1)
+    
+    response = requests.get(url)
+    json_body = response.json()
+    
+    for i in range(0, int(json.dumps(json_body[0]["usedIngredientCount"]))):
+        print(json.dumps(json_body[0]["usedIngredients"][i]["original"], indent=2))
+        ingredientList.append(json.dumps(json_body[0]["usedIngredients"][i]["original"]).strip("\""))
+
+    for i in range(0, int(json.dumps(json_body[0]["missedIngredientCount"]))):
+        print(json.dumps(json_body[0]["missedIngredients"][i]["original"], indent=2))
+        ingredientList.append(json.dumps(json_body[0]["missedIngredients"][i]["original"]).strip("\""))
+        
+    print(json.dumps(json_body[0]["title"], indent=2))
+    title=json.dumps(json_body[0]["title"], indent=2)
+    title=title.strip("\"")
+    print(json.dumps(json_body[0]["image"]))
+    imageURL=json.dumps(json_body[0]["image"])
+    
       
     for status in tweets:
         try:
@@ -43,7 +78,7 @@ def index():
         if status.created_at < end_date:
             break
       
-    return flask.render_template("index.html", len = len(tweetList), tweetList = tweetList, userList=userList, dateList=dateList)
+    return flask.render_template("index.html", len = len(ingredientList), tweetList = tweetList, userList=userList, dateList=dateList, ingredientList=ingredientList, title=title,imageURL=imageURL)
 
 # if __name__ == "__main__":
 app.run(
